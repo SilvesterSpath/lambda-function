d54ff367a8ee1064395129acd19ee7ca820e2d3f
SilvesterSpath <spath.sz@gmail.com>
2024-04-03 11:17:03 +0200

    lambda function

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,9 +1,19 @@
 exports.handler = function (event, context, callback) {
-  callback(null, {
-    stausCode: 200,
+  console.log('event', event);
+
+  const { name } = JSON.parse(event.body);
+  console.log('parsed ', name);
+
+  /*   callback(null, {
+    statusCode: 200,
     body: JSON.stringify({
       message: 'Go Serverless v1.0! Your function executed successfully!',
       input: event,
     }),
+  }); */
+
+  callback(null, {
+    statusCode: 200,
+    body: 'Hello ' + name,
   });
 };
