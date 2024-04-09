29a14190f72bc2a5f15bd6cb2dafda0de668c2ea
SilvesterSpath <spath.sz@gmail.com>
2024-04-09 09:46:41 +0200

    axios error

--- functions/getusers.js
+++ functions/getusers.js
@@ -1,13 +1,54 @@
+const axios = require('axios');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 exports.handler = function (event, context, callback) {
+  const API_URL = 'https://api.github.com/users';
+  const API_ID = process.env.GITHUB_CLIENT_ID;
+  const API_SECRET = process.env.GITHUB_CLIENT_SECRET;
+
+  const URL = `${API_URL}?client_id=${API_ID}&client_secret=${API_SECRET}`;
+
+  // Send user response
+  const send = (body) => {
+    callback(null, {
+      statusCode: 200,
+      body: JSON.stringify(body),
+    });
+  };
+
+  // Perform API call
+  const getUsers = async () => {
+    try {
+      const response = await axios.get(URL);
+      console.log('response', response.data);
+      send(response.data);
+    } catch (error) {
+      console.error(error);
+      send(error);
+    }
+  };
+
+  // Make sure the method is GET
+  if (event.httpMethod === 'GET') {
+    getUsers();
+  } else {
+    send({ error: 'Method not allowed' });
+  }
+};
+
+/* exports.handler = function (event, context, callback) {
   console.log('event', event);
 
   if (event.httpMethod === 'POST') {
+    console.log(process.env.GITHUB_CLIENT_ID);
     try {
       const parsedBody = JSON.parse(event.body);
       const { name } = parsedBody;
       callback(null, {
         statusCode: 200,
-        body: 'Hello ' + name,
+        body: JSON.stringify({ msg: 'Hello' + name }),
       });
     } catch (error) {
       // Handle parsing errors (e.g., invalid JSON)
@@ -21,4 +62,4 @@ exports.handler = function (event, context, callback) {
       body: 'Try it in postman',
     });
   }
-};
+}; */
